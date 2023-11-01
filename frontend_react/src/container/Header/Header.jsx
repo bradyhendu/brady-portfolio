afce8e055895a11003911b2ce4f20240a4855a24
Brady Henderson <bradyhendu@gmail.com>
2023-11-01 13:11:39 -0400

    Adds framer motion Header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Header.scss'
+import {images} from '../../constants'
+import{motion} from 'framer-motion'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className ="app__header app__flex">
+      <motion.div
+      whileInView={{x: [-100, 0], opacity:[0,1]}}
+      transition={{duratioin: 0.5}}
+      className="app__header-info"
+      >
+        <div className='a==__header-badge'>
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{marginLeft: 20}}>
+              <p className='p-text'>Hi, I am</p>
+              <h1 className='head-text'>Brady Henderson</h1>
+            </div>
+          </div>
+        </div>
+      </motion.div>
     </div>
   )
 }
