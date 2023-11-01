8afcda91e42961236c113ec221a92e8eadb744d3
Brady Henderson <bradyhendu@gmail.com>
2023-10-31 22:58:41 -0400

    Imports out App.scss file

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
 import {About, Footer, Header, Skills, Testimonial, Work} from './container';
+import {Navbar} from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <Header />
-        <About />
-        <Work />
-        <Skills />
-        <Testimonial />
-        <Footer />
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
