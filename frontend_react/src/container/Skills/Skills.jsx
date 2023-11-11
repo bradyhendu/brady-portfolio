e8b056f29085c2eba06f46842f93c195b43621ef
Brady Henderson <bradyhendu@gmail.com>
2023-11-10 22:46:39 -0500

    adds skill imports

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Skills.scss'
-
+import {motion} from 'framer-motion'
+import { urlFor, client } from '../../client'
+import {AppWrap} from '../../wrapper'
+import ReactTooltip from 'react-tooltip'
 const Skills = () => {
   return (
     <div>
-      Skills
+      
     </div>
   )
 }
