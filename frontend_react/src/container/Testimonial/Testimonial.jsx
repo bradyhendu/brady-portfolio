f6257e2ffd2001bd45d92b2184c91694998639f5
Brady Henderson <bradyhendu@gmail.com>
2023-11-12 10:44:17 -0500

    Starts testimonials

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {motion} from 'framer-motion'
+
 import './Testimonial.scss'
+import { urlFor, client } from '../../client'
+import {AppWrap, MotionWrap} from '../../wrapper'
+
+
+
 
 const Testimonial = () => {
+  const[brands, setBrands] = useState([]);
+  const[testimonials, setTestimonials] = useState([]);
+  const[currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query).then((data)=>{
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery).then((data)=>{
+      setBrands(data);
+    });
+  }, [])
+
   return (
-    <div>
-      Testimonial
-    </div>
+    <>
+      {testimonials.length && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={urlFor(testimonials[currentIndex].imgurl)} alt='testimonial'/>
+          </div>  
+        </>
+      )}
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(MotionWrap(Testimonial,'app__testimonial'), 'testimonial', "app__primarybg")
