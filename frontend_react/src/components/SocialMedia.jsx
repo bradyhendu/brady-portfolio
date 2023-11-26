62a238759fb4550196aaa6da6a988b8268441cd4
Brady Henderson <bradyhendu@gmail.com>
2023-11-26 18:15:22 -0500

    Clicking on the social media btns work

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,15 +4,21 @@ import {BsTwitter, BsInstagram, BsLinkedin} from 'react-icons/bs'
 const SocialMedia = () => {
     return(
         <div className="app__social">
-            <div>
-                <BsTwitter />
-            </div>
-            <div>
-                <BsInstagram />
-            </div>
-            <div>
-                <BsLinkedin />
-            </div>
+            <a href= "https://twitter.com/Bradyhendu" target="_blank" rel="noreferrer">
+                <div onclick="location.href='https://twitter.com/Bradyhendu';">
+                    <BsTwitter />
+                </div>
+            </a>
+            <a href= "https://www.instagram.com/bradyhendu/?next=%2F" target="_blank" rel="noreferrer">
+                <div>
+                    <BsInstagram />
+                </div>
+            </a>
+            <a href= "https://www.linkedin.com/in/bradyhendu/" target="_blank" rel="noreferrer">
+                <div>
+                    <BsLinkedin />
+                </div>
+            </a>
         </div>
     )
 }
